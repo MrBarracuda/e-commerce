d4e4d6dbd4e285d241af81cf1c5575a6f693b341
Dmytro Dobrovolskyi <1dima9999@gmail.com>
2024-09-02 17:41:14 -0400

    setup basic libraries

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -4,10 +4,11 @@
 import { sql } from "drizzle-orm";
 import {
   index,
-  pgTableCreator,
   serial,
   timestamp,
   varchar,
+  uuid,
+  pgTable,
 } from "drizzle-orm/pg-core";
 
 /**
@@ -16,9 +17,9 @@ import {
  *
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
-export const createTable = pgTableCreator((name) => `e-commerce_${name}`);
+// export const createTable = pgTableCreator((name) => `e-commerce_${name}`);
 
-export const posts = createTable(
+export const posts = pgTable(
   "post",
   {
     id: serial("id").primaryKey(),
@@ -27,10 +28,26 @@ export const posts = createTable(
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
     updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-      () => new Date()
+      () => new Date(),
     ),
   },
   (example) => ({
     nameIndex: index("name_idx").on(example.name),
-  })
+  }),
 );
+
+export const users = pgTable("users", {
+  // You can use { mode: "bigint" } if numbers are exceeding js number limitations
+  id: uuid("id").primaryKey(),
+  avatar: varchar("avatar"),
+  fullName: varchar("first_name"),
+  // lastName: varchar("last_name"),
+  username: varchar("username").notNull(),
+  email: varchar("email").notNull(),
+  password: varchar("password"),
+  // dateOfBirth: date("date_of_birth"),
+  phone: varchar("phone_number"),
+  createdAt: timestamp("created_at", { withTimezone: true, mode: "string" })
+    .defaultNow()
+    .notNull(),
+});
