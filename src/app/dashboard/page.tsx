6ff2658b7fb50c5aa39c67a8105fbf85bc1a86cd
Dmytro Dobrovolskyi <1dima9999@gmail.com>
2024-09-04 17:21:24 -0400

    add dashboard and profile pages

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Wrapper } from "@/components/wrapper";
+
+export default function Dashboard() {
+  return (
+    <>
+      <Wrapper className="py-20">
+        <div className="grid-2 grid">
+          <h1 className="text-3xl">Dashboard</h1>
+          <Link
+            href="/dashboard/create"
+            className={cn(buttonVariants({ variant: "link" }), "text-lg")}
+          >
+            Create
+          </Link>
+        </div>
+      </Wrapper>
+    </>
+  );
+}
